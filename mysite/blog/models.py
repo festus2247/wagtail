e65d3035abcf5574961efcf16e6287649ee53ffc
festus <festusolorunfemi3@gmail.com>
2024-06-03 05:43:19 -0700

    initial commit

---
+++ mysite/blog/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+# Add these:
+from wagtail.models import Page
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel
+
+
+class BlogIndexPage(Page):
+    intro = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro', classname="full")
+    ]
+
+    def get_context(self, request):
+        # Update context to include only published posts, ordered by reverse-chron
+        context = super().get_context(request)
+        blogpages = self.get_children().live().order_by('-first_published_at')
+        context['blogpages'] = blogpages
+        return context
+
+
+class Blogpage(Page):
+    date = models.DateField("Post date")
+    intro = models.CharField(max_length=250)
+    body = RichTextField(blank = True)
+    image = models.ForeignKey(
+        'wagtailimages.Image', on_delete=models.CASCADE, related_name='+', blank=True, null=True
+    )
+    caption = models.CharField(blank=True, max_length=250)
+
+
+    content_panels = Page.content_panels + [
+        FieldPanel('date'),
+        FieldPanel('intro', ),
+        FieldPanel('body', classname="full"),
+        FieldPanel('image'),
+        FieldPanel('caption'),
+    ]
+    
